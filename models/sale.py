1e881ecebdfbab5a0a71a0c3caf8a2df1e7c06f1
Abimanyu BP <abimanyubevanp@gmail.com>
2022-04-02 15:36:01 +0700

    [IMP] sale_mod: added auto create MO function
    Menambahkan fungsi agar button bisa membuat entry MO baru
    dengan produk_id dan qty yang sesuai dengan entry SO yang
    dibuat.

--- models/sale.py
+++ models/sale.py
@@ -5,4 +5,19 @@ class SaleInherited(models.Model):
     _inherit = 'sale.order'
 
     def createMoAuto(self):
-        pass
+        target = []
+        target_browse = self.env['sale.order'].browse(self.id)
+        for k in target_browse:
+            target.append([{
+                'product_id':k.order_line.product_id.id,
+                'bom_id':k.order_line.product_id.bom_ids.id,
+                'hrg_bom':k.order_line.product_id.bom_ids.hrg_bom,
+                'product_qty':k.order_line.product_uom_qty,
+                'product_uom_id':k.order_line.product_id.bom_ids.product_uom_id.id,
+            }])
+        tar_product_id = target[0][0]["product_id"]
+        tar_product_qty = target[0][0]["product_qty"]
+        tar_bom_id = target[0][0]["bom_id"]
+        tar_hrg_bom = target[0][0]["hrg_bom"]
+        tar_product_uom_id = target[0][0]["product_uom_id"]
+        self.env['mrp.production'].create({'product_id': tar_product_id, 'product_uom_id': tar_product_uom_id, 'bom_id': tar_bom_id, 'product_qty': tar_product_qty})

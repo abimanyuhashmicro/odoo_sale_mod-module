e7b5b9a2ba6ed22520f1939ab6ce8ab479e3eea1
Abimanyu BP <abimanyubevanp@gmail.com>
2022-04-04 13:12:23 +0700

    [REM] sale_mod: remove redundant list

--- models/sale.py
+++ models/sale.py
@@ -8,18 +8,18 @@ class SaleInherited(models.Model):
         target = []
         target_browse = self.env['sale.order'].browse(self.id)
         for k in target_browse:
-            target.append([{
+            target.append({
                 'product_id':k.order_line.product_id.id,
                 'bom_id':k.order_line.product_id.bom_ids.id,
                 'hrg_bom':k.order_line.product_id.bom_ids.hrg_bom,
                 'product_qty':k.order_line.product_uom_qty,
                 'product_uom_id':k.order_line.product_id.bom_ids.product_uom_id.id,
-            }])
-        tar_product_id = target[0][0]["product_id"]
-        tar_product_qty = target[0][0]["product_qty"]
-        tar_bom_id = target[0][0]["bom_id"]
-        tar_hrg_bom = target[0][0]["hrg_bom"]
-        tar_product_uom_id = target[0][0]["product_uom_id"]
+            })
+        tar_product_id = target[0]["product_id"]
+        tar_product_qty = target[0]["product_qty"]
+        tar_bom_id = target[0]["bom_id"]
+        tar_hrg_bom = target[0]["hrg_bom"]
+        tar_product_uom_id = target[0]["product_uom_id"]
         if tar_hrg_bom > 0:
             self.env['mrp.production'].create({'product_id': tar_product_id, 'product_uom_id': tar_product_uom_id, 'bom_id': tar_bom_id, 'product_qty': tar_product_qty})
         else:
